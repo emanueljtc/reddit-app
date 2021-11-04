3c7606fe82698f51d9fcddc75fbd9d0da0cd82c3
Ing Emanuel Torres <emanueljtc@gmail.com>
2021-11-04 18:41:39 -0400

    remove text

--- screens/Top/index.js
+++ screens/Top/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { ItemNotice } from '../../components';
 import RedditServices from '../../utils/services/RedditServices';
 import { PageScrollView } from 'pagescrollview';
